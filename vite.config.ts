7c381dacc07c772d11a5b709badc53e90e21c8e8
JedWoj <jedwojc@gmail.com>
2024-09-03 12:34:10 +0200

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import packageJson from './package.json';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: true,
+    sourcemap: false,
+  },
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
 });
